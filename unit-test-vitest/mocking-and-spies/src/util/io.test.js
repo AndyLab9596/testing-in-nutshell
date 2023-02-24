d079f233c562c240d2505098a23680d9592ce9f5
vy.ct <vyct@mor.com.vn>
2023-02-24 11:36:49 +0700

    [update] Custom mock

--- unit-test-vitest/mocking-and-spies/src/util/io.test.js
+++ unit-test-vitest/mocking-and-spies/src/util/io.test.js
@@ -3,13 +3,23 @@ import writeData from './io';
 import { promises as fs } from 'fs';
 
 vi.mock('fs');
+vi.mock('path', () => {
+    return {
+        default: {
+            join(...args) {
+                return args[args.length - 1];
+            }
+        }
+    }
+});
 
 it('should execute the writeFile method', () => {
-    const testData = "";
+    const testData = "Test";
     const testFilename = "test.txt";
-    
+
     writeData(testData, testFilename)
     // return expect(writeData(testData, testFilename)).resolves.toBeUndefined()
-    expect(fs.writeFile).toBeCalled();
+    // expect(fs.writeFile).toBeCalled();
+    expect(fs.writeFile).toBeCalledWith(testFilename, testData);
 
 });
