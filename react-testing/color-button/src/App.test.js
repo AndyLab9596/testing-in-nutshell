66ff5066cd8ee04e3a3a7f9ae32d4f5a757b7e1f
andyvy <thienvy95@gmail.com>
2023-02-25 08:56:34 +0700

    Frist Jest Test with RTL

---
+++ react-testing/color-button/src/App.test.js
@@ -0,0 +1,19 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import App from "./App";
+
+test('button has correct initial color, and update when clicked', () => {
+  // const { container } = render(<App />);
+  // logRoles(container);
+
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' })
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' })
+  expect(colorButton).toHaveTextContent('Change to red');
+});
