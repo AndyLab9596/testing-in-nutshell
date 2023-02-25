d2aae9ac469fc2c54378575c8fe488620834668d
andyvy <thienvy95@gmail.com>
2023-02-25 14:08:18 +0700

    Testing popover

--- react-testing/sundaes-on-demand/src/pages/test/SummaryForm.test.js
+++ react-testing/sundaes-on-demand/src/pages/test/SummaryForm.test.js
@@ -15,4 +15,21 @@ test('initial state of checkbox, and update when clicked', async () => {
 
     expect(checkbox).toBeChecked();
     expect(confirmButton).toBeEnabled();
+});
+
+test('popover responds to hover', async () => {
+    const user = userEvent.setup();
+    render(<SummaryForm/>);
+    // popover starts out hidden
+    const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+    expect(nullPopover).not.toBeInTheDocument();
+    // popover appears on mouseover of checkbox label
+    const termAndConditions = screen.getByText(/terms and conditions/i);
+    await user.hover(termAndConditions);
+    const popover = screen.getByText(/no ice cream will actually be delivered/i);
+    expect(popover).toBeInTheDocument();
+    // popover disappers when mouse out
+    await user.unhover(termAndConditions);
+    expect(popover).not.toBeInTheDocument();
+
 })
