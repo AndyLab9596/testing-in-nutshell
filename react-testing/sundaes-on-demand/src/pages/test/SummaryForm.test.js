d139ba816102aacdf3fc19b371f32884ec66eaa1
andyvy <thienvy95@gmail.com>
2023-02-25 13:47:34 +0700

    Change fireEvent to userEvent

--- react-testing/sundaes-on-demand/src/pages/test/SummaryForm.test.js
+++ react-testing/sundaes-on-demand/src/pages/test/SummaryForm.test.js
@@ -1,13 +1,17 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../summary/SummaryForm.jsx";
 
-test('initial state of checkbox, and update when clicked', () => {
+test('initial state of checkbox, and update when clicked', async () => {
+    const user = userEvent.setup();
+
     render(<SummaryForm />);
     const checkbox = screen.getByRole('checkbox', { name: /terms and conditions/i });
     const confirmButton = screen.getByRole('button', { name: /confirm order/i });
     expect(checkbox).not.toBeChecked();
     expect(confirmButton).toBeDisabled();
-    fireEvent.click(checkbox);
+
+    await user.click(checkbox)
 
     expect(checkbox).toBeChecked();
     expect(confirmButton).toBeEnabled();
