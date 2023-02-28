315e93a4d945bbe7de01e1f5cd29ff9d6315b130
andyvy <thienvy95@gmail.com>
2023-02-28 10:43:05 +0700

    Update ToppingOption unit test

--- react-testing/sundaes-on-demand/src/pages/entry/test/Options.test.js
+++ react-testing/sundaes-on-demand/src/pages/entry/test/Options.test.js
@@ -13,3 +13,14 @@ test('display image for each scoop option from server', async () => {
 
     expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 })
+
+test('display image for each topping option from server', async () => {
+    render(<Options optionType="toppings" />)
+
+    const toppingImages = await screen.findAllByRole('img', { name: /topping$/i });
+    expect(toppingImages).toHaveLength(3)
+
+    const altText = toppingImages.map((elemnt) => elemnt.alt);
+
+    expect(altText).toEqual(['Cherries topping', 'M&Ms topping', 'Hot fudge topping']);
+})
